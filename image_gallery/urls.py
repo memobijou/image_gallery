7c5329004fbfadfaf755cedc9c66f4dbfec4e0ed
Mimoun Bijjou <mimoun.bijjou@node.energy>
2018-02-17 15:49:37 +0100

    added bootstrap3 and image-gallery theme

--- image_gallery/urls.py
+++ image_gallery/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('gallery/', TemplateView.as_view(template_name="gallery.html")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
