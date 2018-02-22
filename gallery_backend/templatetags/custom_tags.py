01c67a4760fe43a406030e9133cbcef576dbb3ec
Mimoun Bijjou <mimoun.bijjou@node.energy>
2018-02-23 00:07:58 +0100

    added filter and so on

--- gallery_backend/templatetags/custom_tags.py
+++ gallery_backend/templatetags/custom_tags.py
@@ -9,5 +9,12 @@ def getattr_(obj, k):
 
 
 @register.filter
+def get_dict(obj, k):
+    if k in obj:
+        return obj[k]
+    else:
+        return ""
+
+@register.filter
 def get_image_path_from_object(obj, k):
     return getattr(obj, k).url
--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views import generic
 from product import models
 # Create your views here.
@@ -7,4 +8,23 @@ class ProductListView(generic.ListView):
     template_name = "gallery.html"
 
     def get_queryset(self):
-        return models.Product.objects.all()
+        query = Q()
+        for k,v in self.request.GET.items():
+            if v:
+                query &= Q(**{f"{k}__icontains": v})
+        return models.Product.objects.filter(query)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["field_names"] = self.get_field_names(exclude=["id"])
+        return context
+
+    def get_field_names(self,exclude=None):
+        fields = models.Product._meta.get_fields()
+        field_names = []
+        for field in fields:
+            if exclude:
+                if field.name in exclude:
+                    continue
+            field_names.append((field.name, field.verbose_name))
+        return field_names
