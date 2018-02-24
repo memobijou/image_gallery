cbf6034f294655f5920e5c274cfff66197286a51
Mimoun Bijjou <mimoun.bijjou@node.energy>
2018-02-24 13:35:31 +0100

    changed model

--- product/models.py
+++ product/models.py
@@ -5,5 +5,10 @@ from django.db import models
 
 class Product(models.Model):
     image = models.ImageField(verbose_name="Bild", null=True, blank=True)
-    title = models.CharField(verbose_name="Titel", max_length=200)
-    description = models.TextField(verbose_name="Beschreibung", max_length=200)
+    title = models.CharField(verbose_name="Titel", max_length=200, null=True, blank=True)
+    supplier = models.CharField(verbose_name="Hersteller", max_length=200, null=True, blank=True)
+    description = models.TextField(verbose_name="Beschreibung", max_length=200, null=True, blank=True)
+    ean = models.CharField(verbose_name="EAN", max_length=200, null=True, blank=True)
+    amount = models.CharField(verbose_name="Menge", max_length=200, null=True, blank=True)
+    price = models.CharField(verbose_name="Preis", max_length=200, null=True, blank=True)
+    purchase_price = models.CharField(verbose_name="2018", max_length=200, null=True, blank=True)
